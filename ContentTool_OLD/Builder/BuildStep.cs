ae15b815919fff0b712b77d29668f4ca1c25536d
Fabian Nawratil <fabian@nawratil.me>
2017-05-26 15:35:56 +0200

    Created new ContentTool basics

---
+++ ContentTool_OLD/Builder/BuildStep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ContentTool.old.Builder
+{
+    [Flags()]
+    public enum BuildStep
+    {
+        Clean=1,
+        Build=2,
+        Abort=4,
+        Finished=8,
+        Built=16
+    }
+}
+
