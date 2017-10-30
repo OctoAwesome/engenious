f07a331be4f86e8d68145218b2a8d1bbace69af7
Lassi <csharplassi@posteo.de>
2017-10-30 19:41:32 +0100

    Add Vector3d

--- engenious/Converters/VectorConverters.cs
+++ engenious/Converters/VectorConverters.cs
@@ -78,6 +78,33 @@ namespace engenious
             return base.ConvertFrom(context, culture, value);
         }
     }
+    
+    public class Vector3dConverter : ExpandableObjectConverter
+    {
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var s = value as string;
+            if (s == null)
+                return base.ConvertFrom(context, culture, value);
+            var val = s.Trim();
+            if (val.StartsWith("[") && val.EndsWith("]"))
+                val = val.Substring(1,val.Length-2);
+            var splt = val.Split(new[]{','},3,StringSplitOptions.RemoveEmptyEntries);
+            double x,y,z;
+            if (double.TryParse(splt[0].Trim(),NumberStyles.Float,CultureInfo.InvariantCulture.NumberFormat,out x) 
+                && double.TryParse(splt[1].Trim(),NumberStyles.Float,CultureInfo.InvariantCulture.NumberFormat,out y) 
+                && double.TryParse(splt[2].Trim(),NumberStyles.Float,CultureInfo.InvariantCulture.NumberFormat,out z))
+            {
+                return new Vector3d(x,y,z);
+            }
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+    
     public class Vector4Converter : ExpandableObjectConverter
     {
         public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
