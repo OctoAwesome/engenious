c54d3d7c309df95446fb5cec2d7c092f264bd8c4
jvbsl <jvbsl@novacyb.com>
2018-03-31 13:31:58 +0200

    new opentk with ugly PointToClient/Screen fix for SDL2 with X11

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -7,9 +7,9 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("jvbsl")]
 [assembly: AssemblyProduct("engenious")]
-[assembly: AssemblyVersion("0.1.38.0")]
+[assembly: AssemblyVersion("0.1.39.0")]
 [assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.38+Branch.develop.Sha.0c6531c7235fad3e0d004d87ccd2299c7a4d9aed")]
+[assembly: AssemblyInformationalVersion("0.1.39+Branch.develop.Sha.6d2d7efcf161355e74052deda10fa4e9158d0f8f")]
 [assembly: AssemblyCopyright("Copyright (c) jvbsl 2016")]
 [assembly: AssemblyConfiguration("Release")]
 
